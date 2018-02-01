db521515ec00251a0afec1fa28a5350e44b913fb
selvacsk <35958788+selvacsk@users.noreply.github.com>
2018-02-01 16:29:41 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<coniuo.h>
+void main()
+{
+int c;
+printf("enter the variable of c:");
+scanf("%d",&c);
+if(c%2==0)
+{
+printf("the given number is even");
+}
+else
+{
+printf("the given number is odd");
+}
+}
