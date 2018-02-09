297e80ae354879f342d25a02839599cbe68bff73
selvacsk <35958788+selvacsk@users.noreply.github.com>
2018-02-09 10:48:05 +0000

    Create integer.c

---
+++ integer.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n,k,i,h,sum=0;
+printf("enter the values:");
+scanf("%d\t%d",&n,&k);
+for(i=1;i<=n;i++)
+{
+printf("%d\n",n);
+}
+for(h=1;h<=k;h++)
+{
+sum=sum+h;
+}
+printf("%d is sum",sum);
+return 0;
+}
