ccb68d5e609c06469949a95f2282497b14b839e7
selvacsk <35958788+selvacsk@users.noreply.github.com>
2018-02-27 16:40:51 +0530

    Update integer.c

--- integer.c
+++ integer.c
@@ -1,19 +1,15 @@
 
-#include <stdio.h>
-int main()
+#include<stdio.h>
+int main(void)
 {
-    long long n;
-    int count = 0;
-
-    printf("Enter an integer: ");
-    scanf("%lld", &n);
-
-    while(n != 0)
-    {
-        // n = n/10
-        n /= 10;
-        ++count;
-    }
-
-    printf("Number of digits: %d", count);
+int a;
+int count=0;
+scanf("%d",&a);
+while(a!=0)
+{
+a/=10;
+++count;
+}
+printf("%d",count);
+return 0;
 }
