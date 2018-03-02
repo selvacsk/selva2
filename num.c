99be2de62f90025e1d98f08dfbcdca2184e112c7
selvacsk <35958788+selvacsk@users.noreply.github.com>
2018-03-02 15:07:52 +0530

    Create num.c

---
+++ num.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int j,o,u;
+printf("enter the value for o,u");
+scanf("%d %d",&o, &u);
+for(j=0+1; j<=u; j++)
+{
+    if(j%2!=0)
+    printf("d\n",j);
+}
+}
